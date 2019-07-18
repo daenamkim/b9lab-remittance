f232e3183275eee7cd3f623a8dcfcefeab77a1f9
Daenam Kim <daenam.kim@gmail.com>
2019-07-18 12:47:49 +0900

    Add exchange shop

---
+++ ethereum/contracts/ExchangeShop.sol
@@ -0,0 +1,51 @@
+pragma solidity 0.5.10;
+
+import './Pausable.sol';
+
+contract Remittance {
+  function remit(string memory password1, string memory password2) public;
+  function withdrawed(address receiver) external;
+}
+
+contract ExchangeShop is Pausable {
+  mapping(address => uint) balances;
+  mapping(bytes32 => address) senders;
+
+  function exchange(
+    Remittance remittanceContract,
+    string memory password1,
+    string memory password2,
+    address receiver
+  ) public onlyOwner whenNotPaused {
+    bytes32 key = keccak256(abi.encodePacked(this, receiver, password1));
+    senders[key] = address(remittanceContract);
+
+    // Request Alice to take value with two-pair passwords
+    (bool ok,) = address(remittanceContract).call(
+      abi.encodeWithSignature('remit(string,string,address)', password1, password2, receiver)
+    );
+    require(ok, 'Remit should be called');
+  }
+
+  function updateBalance(address receiver, uint value) external whenNotPaused {
+    require(receiver != address(0), 'Receiver should have an address');
+    require(value > 0, 'Value should be greater than 0');
+
+    balances[receiver] = value;
+  }
+
+  function withdraw(string memory password) public whenNotPaused {
+    bytes32 key = keccak256(abi.encodePacked(this, msg.sender, password));
+    require(senders[key] != address(0), 'Receiver with password should be mapped to original sender');
+    require(balances[msg.sender] > 0, 'Should have some balance');
+
+    msg.sender.transfer(balances[msg.sender]);
+    balances[msg.sender] = 0;
+
+    // Notify to Alice
+    (bool ok,) = address(senders[key]).call(
+      abi.encodeWithSignature('withdrawed(address)', msg.sender)
+    );
+    require(ok, 'withdrawed should be called');
+  }
+}
--- ethereum/contracts/Remittance.sol
+++ ethereum/contracts/Remittance.sol
@@ -2,6 +2,10 @@ pragma solidity 0.5.10;
 
 import './Pausable.sol';
 
+contract ExchangeShop {
+  function updateBalance(address receiver, uint value) public;
+}
+
 contract Remittance is Pausable {
     mapping(bytes32 => uint) private _passwordKeyStore;
     mapping(bytes32 => uint) public balances;
@@ -15,7 +19,8 @@ contract Remittance is Pausable {
       _seed2 = seed2;
     }
 
-    event LogRemit(address sender);
+    event LogRemit(address indexed sender);
+    event LogWithdrawed(address indexed receiver);
 
     function _generateKey(
       string memory seed1,
@@ -23,7 +28,6 @@ contract Remittance is Pausable {
       string memory seed2,
       string memory password2
     ) private view returns (bytes32) {
-      // NOTE: password1, password2 will be passed from outside
       bytes32 password1Encoded = keccak256(abi.encodePacked(this, seed1, password1));
       bytes32 password2Encoded = keccak256(abi.encodePacked(this, seed2, password2));
       return keccak256(abi.encodePacked(this, password1Encoded, password2Encoded));
@@ -39,7 +43,7 @@ contract Remittance is Pausable {
       require(password1Check != password2Check, 'Each password should be different');
 
       bytes32 key = _generateKey(_seed1, password1, _seed2, password2);
-      require(_passwordKeyStore[key] == 0, 'One of passwords is used in the key store');
+      require(_passwordKeyStore[key] == 0, 'Same password pair should not be used');
 
       _passwordKeyStore[key] = now + expire;
       balances[key] += msg.value;
@@ -48,21 +52,32 @@ contract Remittance is Pausable {
     function checkKey(
       string memory password1,
       string memory password2
-    ) public onlyOwner whenNotPaused view returns (uint, uint, bytes32) {
+    ) public onlyOwner view returns (uint, uint, bytes32) {
       bytes32 key = _generateKey(_seed1, password1, _seed2, password2);
       return (_passwordKeyStore[key], now, key);
     }
 
-    function remit(string memory password1, string memory password2) public whenNotPaused {
+    function remit(string memory password1, string memory password2, address receiver) public whenNotPaused returns (uint) {
       bytes32 key = _generateKey(_seed1, password1, _seed2, password2);
       require(_passwordKeyStore[key] != 0, 'Key must be in the key store');
       require(_passwordKeyStore[key] > now, 'Key should not be expired');
       require(balances[key] > 0, 'Balance should be greater than 0');
+      require(receiver != address(0), 'Reciever shoudl have an address');
 
       msg.sender.transfer(balances[key]);
+      // Send value information of receiver
+      bool ok = ExchangeShop(msg.sender).call(
+        abi.encodeWithSignature('updateBalance(address,uint)', receiver, balances[key])
+      );
+      require(ok, 'updateBalance should be called');
+
       balances[key] = 0;
       _passwordKeyStore[key] = 0;
       emit LogRemit(msg.sender);
+    }
+
+    function withdrawed(address receiver) external {
+      emit LogWithdrawed(receiver);
     }
 
     function setSeeds(string memory seed1, string memory seed2) public onlyOwner whenNotPaused {
@@ -70,7 +85,7 @@ contract Remittance is Pausable {
       _seed2 = seed2;
     }
 
-    function getSeeds() public onlyOwner whenNotPaused view returns (string memory, string memory) {
+    function getSeeds() public onlyOwner view returns (string memory, string memory) {
       return (_seed1, _seed2);
     }
 }
