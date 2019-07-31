75c3c14fe882f7ec219d0daccc96e32bc6d78fa4
Daenam Kim <daenam.kim@gmail.com>
2019-07-31 14:00:42 +0900

    Add change owner procedure

--- contracts/Ownable.sol
+++ contracts/Ownable.sol
@@ -2,10 +2,16 @@ pragma solidity 0.5.10;
 
 contract Ownable {
     address private _owner;
+    address private _ownerCandidate;
 
     constructor() public {
         _owner = msg.sender;
+        _ownerCandidate = address(0);
     }
+
+    event LogRequestOwnerCandidate(address candidate);
+    event LogAcceptOwnerCandidate(address candidate);
+    event LogRevokeOwnerCandidate(address candidate);
 
     modifier onlyOwner() {
         require(msg.sender == _owner, "Should be only owner");
@@ -15,5 +21,32 @@ contract Ownable {
 
     function getOwner() public view returns (address) {
         return _owner;
+    }
+
+    function requestOwnerCandidate(address ownerCandidate) public onlyOwner returns (bool) {
+        _ownerCandidate = ownerCandidate;
+
+        emit LogRequestOwnerCandidate(ownerCandidate);
+
+        return true;
+    }
+
+    function acceptOwnerCandidate() public returns (bool) {
+        require(msg.sender != address(0), "Sender address should be valid");
+        require(msg.sender == _ownerCandidate, "Sender should be owner candidate");
+
+        _owner = msg.sender;
+
+        emit LogAcceptOwnerCandidate(msg.sender);
+
+        return true;
+    }
+
+    function revokeOwnerCandidate() public onlyOwner returns (bool) {
+        emit LogRevokeOwnerCandidate(_ownerCandidate);
+
+        _ownerCandidate = address(0);
+
+        return true;
     }
 }
